241b6c23cbcc04dc0d09608cd3fb5d2c6317a5eb
KnDmitriy <99825353+KnDmitriy@users.noreply.github.com>
2023-04-03 11:26:49 +0400

    Add files via upload

---
+++ Graph2/Graph2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph(@"C:\Users\konorovda\Desktop\File input output\Graph2\input1.txt");
+            g.SearchG();
+
+            Console.ReadKey();
+
+        }
+    }
+}
